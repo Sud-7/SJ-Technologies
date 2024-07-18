23cad810d6a9e02a4f8ef612b2e50684cc966629
Sud <155895839+Sud-7@users.noreply.github.com>
2024-07-18 18:00:12 +0530

    Added Linkedin and responsive to Home Font

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,9 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1200px] mx-8 md:mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600 font-bold">Hi, my name is</p>
+        <p className="text-pink-600 font-bold md:text-4xl sm:text-3xl">
+          Hi, my name is
+        </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Sudhashu Jha
         </h1>
