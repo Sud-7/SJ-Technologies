1dbbe320ebfd73a18ee917fbf1f37a4cef0fb773
Sud <155895839+Sud-7@users.noreply.github.com>
2024-07-18 17:54:39 +0530

    Added Resume to the Navbar section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -124,7 +124,7 @@ function NavBar() {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69] rounded-lg">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://drive.google.com/file/d/1FskScaryNSiM7aowVuphhgBn0uYhW1ex/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
